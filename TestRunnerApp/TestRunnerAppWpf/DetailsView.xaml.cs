541981a21c2c20f3d25f21ba8cebb92301921d04
Joakim <joakim.odermalm@gmail.com>
2019-04-30 11:43:45 +0200

    Fix detailview/cycle-tab: Scrollwheel bubbling from datagrid, selectable text in message-column, wrapping without hidden text on narrower detailsview widths.

--- TestRunnerApp/TestRunnerAppWpf/DetailsView.xaml.cs
+++ TestRunnerApp/TestRunnerAppWpf/DetailsView.xaml.cs
@@ -73,6 +73,26 @@ namespace TestRunnerAppWpf
         {
             logTextBox.Clear();
         }
+
+        private void CycleRunDataGridSingle_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                e.Handled = true;
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                eventArg.Source = sender;
+                var parent = ((Control)sender).Parent as UIElement;
+                parent.RaiseEvent(eventArg);
+            }
+        }
+    
+        private void CycleRunDataGridSingle_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+
     }
 
     
