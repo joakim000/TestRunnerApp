83a6a991797e81860b984c9759b510b637cda0d9
Joakim <joakim.odermalm@gmail.com>
2019-05-06 08:55:53 +0200

    Created project for console app

---
+++ TestRunnerApp/TestRunnerAppCon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRunnerAppCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int argCount = args.Length;
+
+            int i = 1;
+            foreach (string s in args)
+            {
+                Console.WriteLine($"arg {i}: [{s}]");
+                i++;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
