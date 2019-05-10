f03cfc5a228f3f1d1f8f7ad3a837bda4cac9372f
Joakim <joakim.odermalm@gmail.com>
2019-05-10 15:51:29 +0200

    Revision increment

--- TestRunnerApp/TestRunnerAppWpf/Properties/Settings.Designer.cs
+++ TestRunnerApp/TestRunnerAppWpf/Properties/Settings.Designer.cs
@@ -262,7 +262,7 @@ namespace TestRunnerAppWpf.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v2.1 rc7")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v2.1 rc9")]
         public string versionString {
             get {
                 return ((string)(this["versionString"]));
